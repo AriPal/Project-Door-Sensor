1751472ab837d367419393c0b97cb1f3ff59f17d
Dler <dleroari@gmail.com>
2014-11-07 09:58:19 +0100

    Update TestFacility.ino

--- TestFacility.ino
+++ TestFacility.ino
@@ -1,6 +1,9 @@
 int ledPin1 = 6; // Red LED for FlameDetector sensor 
 int ledPin2 = 7; // Blue LED for PIR sensor
-int PIR_sensor, Flame_Detector_sensor; // Assign two integers. 
+int PIR_sensor; // Assign integer.
+int pCountInn = 0;
+unsigned int pCountInn = 0; 
+int Flame_Detector_sensor1, Flame_Detector_sensor2; // Assign two integers.  
 
 void setup()
 {
@@ -14,41 +17,43 @@ void loop()
 {
 	
 	// Connect the sensors to analog pins.  
-	PIR_sensor = digitalRead(A0); // From analog to digital (0 and 1)
-	Flame_Detector_sensor = analogRead(A1); // Reads analog 
+// 	PIR_sensor = digitalRead(A0); // From analog to digital (0 and 1)
+	Flame_Detector_sensor1 = analogRead(A0); // Reads analog.
+	Flame_Detector_sensor2 = analogRead(A1); // Reads analog. 
 
 // Only two instance of a static field exists
 
 static long start1 = 0;  // It will be shared by all instances.
 static long start2 = 0;  // .....
 static int timer = 1000; //	 .....
+static int bigtimer = 5000; // ....
 
 
 	
-	if(Flame_Detector_sensor <= 195 && millis() - start1 > timer){
+	if(Flame_Detector_sensor1 <= 500 && millis() - start1 > timer){
 	    	
 	    start1 = millis(); // start1 counter
 	    digitalWrite(ledPin1, HIGH); // Active blue LED. 
 
-	} else if(Flame_Detector_sensor >= 230){ 
+	} else if(Flame_Detector_sensor1 >= 500){ 
 
 		digitalWrite(ledPin1, LOW); // Unactive blue LED.
 
 	} 
 
 // If PIR is equal to 1 and it has gone 1, start clock counter, and set activate blue led. 
-		  if(PIR_sensor == 1 && millis() - start2 > timer){
+		  if(Flame_Detector_sensor2 <= 500 && millis() - start2 > timer){
 		    start2 = millis(); // start2 counter
 		    digitalWrite(ledPin2, HIGH); // Active red LED.
 		   
-		} else if(PIR_sensor == 0) { 
+		} else if(Flame_Detector_sensor2 >= 500) { 
 
 			digitalWrite(ledPin2, LOW); // Unactive blue LED.
 		}
 
 
 // If the start1 timer is less than start2 timer, and start1 counter is active.  
-			if( PIR_sensor == 1 && start1 < start2 && start1 > 0){
+			if(start1 < start2 && start1 > 0){
 				
 				// start table
 				Serial.print("| ");
@@ -58,20 +63,20 @@ static int timer = 1000; //	 .....
 				Serial.print("\t");
 				Serial.print(start2);
 				Serial.print("\t");
-				Serial.print(PIR_sensor);
+				Serial.print(Flame_Detector_sensor1);
 				Serial.print("\t");
-				Serial.print(Flame_Detector_sensor);
+				Serial.print(Flame_Detector_sensor2);
 				Serial.print(" |");
 				Serial.println();
 				// end table
+				pcount++;
 
 				start1 = 0; 
 				start2 = 0; 
+			}
 
-			} 
 
-
-			if(PIR_sensor == 1 && start1 > start2 && start2 > 0){
+			if(start1 > start2 && start2 > 0){
 					
 					// Start table
 					Serial.print("| ");
@@ -81,16 +86,26 @@ static int timer = 1000; //	 .....
 					Serial.print("\t");
 					Serial.print(start2);
 					Serial.print("\t");
-					Serial.print(PIR_sensor);
+					Serial.print(Flame_Detector_sensor1);
 					Serial.print("\t");
-					Serial.print(Flame_Detector_sensor);
+					Serial.print(Flame_Detector_sensor2);
 					Serial.print(" |");
 					Serial.println();	
 					// end table
 
+				pcount--;
+
 				start1 = 0; 
 				start2 = 0; 
 	            
+			}
+
+// If a single sensor is triggered, without 
+// triggering both after 5 seconds (5000 ms), set that sensor timer to zero.   
+			if(start1 > bigtimer){
+			    start1 = 0; 
+			} else if(start2 > bigtimer){
+				start2 = 0; 
 			}
 
 	delay(100); // Wait 100/1000 = 0.1 sec.   
